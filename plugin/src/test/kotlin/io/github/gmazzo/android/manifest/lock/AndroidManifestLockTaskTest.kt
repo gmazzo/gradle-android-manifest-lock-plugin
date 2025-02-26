34fb8bd661b0e2617aa473b544040ba225048736
Guillermo Mazzola <gmazzo65@gmail.com>
2025-02-25 22:27:54 -0300

    Added tests for `AndroidManifestLockTask`

---
+++ plugin/src/test/kotlin/io/github/gmazzo/android/manifest/lock/AndroidManifestLockTaskTest.kt
@@ -0,0 +1,70 @@
+package io.github.gmazzo.android.manifest.lock
+
+import org.gradle.kotlin.dsl.creating
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getValue
+import org.gradle.kotlin.dsl.newInstance
+import org.gradle.kotlin.dsl.register
+import org.gradle.kotlin.dsl.repositories
+import org.gradle.testfixtures.ProjectBuilder
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AndroidManifestLockTaskTest {
+
+    private val tempDir = File(System.getenv("TEMP_DIR")!!).apply { deleteRecursively(); mkdirs() }
+
+    private val manifestFile = tempDir.resolve("AndroidManifest.xml").also {
+        it.outputStream().use(javaClass.getResource("/AndroidManifest.xml")!!.openStream()::copyTo)
+    }
+
+    private val manifestDependencyFile = tempDir.resolve("AndroidManifest.dependecy.jni.yaml").also {
+        it.outputStream().use(javaClass.getResource("/AndroidManifest.dependecy.jni.yaml")!!.openStream()::copyTo)
+    }
+
+    private val manifestLockFile = tempDir.resolve("AndroidManifest.lock.yaml")
+
+    private val task = with(ProjectBuilder.builder()
+        .withProjectDir(tempDir)
+        .withGradleUserHomeDir(tempDir.resolve(".home"))
+        .build()) {
+
+        val debugDependencies by configurations.creating
+
+        dependencies {
+            debugDependencies(files(manifestDependencyFile))
+        }
+
+        tasks.register<AndroidManifestLockTask>("testTask") {
+            variantManifests.put("debug", layout.file(provider { manifestFile }))
+            variantManifests.put("release", layout.file(provider { manifestFile }))
+
+            variantRuntimeClasspath.put("debug", debugDependencies.incoming.artifacts)
+
+            manifestContent.set(objects.newInstance<AndroidManifestLockExtension.Content>().apply {
+                sdkVersion.set(true)
+                configurations.set(true)
+                permissions.set(true)
+                features.set(true)
+                libraries.set(true)
+                nativeLibraries.set(true)
+                exports.set(true)
+            })
+
+            lockFile.set(manifestLockFile)
+            failOnLockChange.set(false)
+        }
+    }
+
+    @Test
+    fun `lock tasks generates a lock file`() {
+        task.get().generateLock()
+
+        assertEquals(
+            javaClass.getResource("/AndroidManifest.lock.yaml")!!.readText(),
+            manifestLockFile.readText(),
+        )
+    }
+
+}
